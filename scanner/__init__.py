de13586b46995351d059f3da9f4eda7e4980e84f
savycode <Shlokchavan812@gmail.com>
2026-04-23 20:40:44 +0530

    Removal of Excess files and fixed bugs

--- scanner/__init__.py
+++
@@ -1,9 +0,0 @@
-# Cyber Hawk - Web Vulnerability Scanner
-# Scanner Package
-
-from .crawler import Crawler
-from .vuln_scanner import VulnScanner
-from .engine import ScanEngine
-from .reporter import Reporter
-
-__all__ = ["Crawler", "VulnScanner", "ScanEngine", "Reporter"]
--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# Cyber Hawk - Utils Package
-from .helpers import sanitize_url, is_valid_url, get_domain
-
-__all__ = ["sanitize_url", "is_valid_url", "get_domain"]
